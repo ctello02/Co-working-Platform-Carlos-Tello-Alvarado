89a423d5035fafded44fde92a3a67d5cde2bc798
Tello <carlostello@usal.es>
2024-10-13 19:38:00 +0200

    project changed to own biolerplate

--- mevn-boilerplate-main/server/classes/response/failed.js
+++
@@ -1,9 +0,0 @@
-const fail = require('./fail')
-
-module.exports = (res, message, status = 200) => {
-  if (message && typeof message === 'object') {
-    message = `<ul><li>${message.join('</li><li>')}</li></ul>`
-  }
-
-  return res.status(status).json(new fail(message))
-}
