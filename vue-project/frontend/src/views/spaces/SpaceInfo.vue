5c6e0b446b0074209849a1ae821a6b9aaa52f79d
Tello <carlostello@usal.es>
2025-06-24 19:11:48 +0200

    Funciona el bulkDelete

--- vue-project/frontend/src/views/spaces/SpaceInfo.vue
+++ vue-project/frontend/src/views/spaces/SpaceInfo.vue
@@ -1,6 +1,10 @@
 <template>
     <SpaceCard :space="space" :adminActions="userStore.getIsAdmin" :reserveActions="true" @go-back="routerBack"
         @reserve="toCreateReservation" @edit-space="openEditSpaceInfo" @delete-space="deleteSpace" />
+
+    <AskModal v-model="bulkDeleteModal" :maxWidth="'600px'" :title="'Este espacio tiene reservas pendientes'"
+        :message="'Este espacio tiene reservas pendientes, ¿Estás seguro de que quieres borrarlo?'"
+        :actionText="'Borrar espacio'" :closeModal="() => bulkDeleteModal = false" :action="bulkDeleteSpace" />
 </template>
 
 <script setup>
@@ -10,13 +14,14 @@ import { useUserStore } from '@/store/userStore';
 import { useSpaceStore } from '@/store/spaceStore';
 import { spaceService } from '@/services/spaceService';
 import SpaceCard from '@/components/SpaceCard.vue';
+import AskModal from '@/components/AskModal.vue';
 
 const router = useRouter();
 const userStore = useUserStore();
 const spaceStore = useSpaceStore();
 
 const space = ref(null);
-const deleteModal = ref(false);
+const bulkDeleteModal = ref(false);
 
 onMounted(() => {
     space.value = spaceStore.getSelectedSpace;
@@ -24,6 +29,33 @@ onMounted(() => {
         router.push('/spaces');
     }
 });
+
+function deleteSpace() {
+    spaceService
+        .deleteSpace(space.value._id)
+        .then((res) => {
+            spaceStore.clearSelectedSpace();
+            routerBack();
+        })
+        .catch((error) => {
+            console.log(error);
+            if (error.response.status === 409) {
+                bulkDeleteModal.value = true;
+            }
+        });
+}
+
+function bulkDeleteSpace() {
+    spaceService.bulkDeleteSpace(space.value._id)
+        .then(res => {
+            bulkDeleteModal.value = false;
+            routerBack();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
 
 function routerBack() {
     router.push('/spaces');
@@ -35,19 +67,5 @@ function openEditSpaceInfo() {
 
 function toCreateReservation() {
     router.push('/createReservation');
-}
-
-function deleteSpace() {
-    spaceService
-        .deleteSpace(space.value._id)
-        .then((res) => {
-            console.log(res.data);
-            deleteModal.value = false;
-            spaceStore.clearSelectedSpace();
-            routerBack();
-        })
-        .catch((error) => {
-            console.log(error);
-        });
 }
 </script>
