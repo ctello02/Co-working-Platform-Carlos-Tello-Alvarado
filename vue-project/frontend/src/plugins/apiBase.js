f6a75a3bb841fb42e98f53dfbbc96aace513672c
Tello <carlostello@usal.es>
2025-08-24 20:02:46 +0200

    Funciona el despliegue

--- vue-project/frontend/src/plugins/apiBase.js
+++ vue-project/frontend/src/plugins/apiBase.js
@@ -5,9 +5,7 @@ export default {
     app.config.globalProperties.$apiBase = apiBase;
     app.config.globalProperties.$resolve = (src) => {
       if (!src) return '';
-      // No toques URLs completas, blobs o data URLs
       if (/^(https?:|blob:|data:)/i.test(src)) return src;
-      // Si empieza por "/" (p.ej. /uploads/...), prefija con API_BASE
       if (src.startsWith('/')) return apiBase + src;
       return src;
     };
